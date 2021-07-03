31349b9eb86817a24c60e48184d663538868f3b7
kion <kion1491@gmail.com>
2021-07-04 05:03:33 +0900

    1. 한 페이지에 여러개의 tab이 있어도 각기 동작하도록 함수화 2. 줄간격 및  font-family 간단한 수정

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 var tabEvtFunc = function(){
-    var tabLi = $("ul.tab_button li"),
-        tabCont = $(".tab_cont");
-
+    var tab = $("ul.tab_button_wrapper"),
+        tabLi = tab.children('li');
     function init(){
         tabLiClickFunc();
     }
-
     function tabLiToggle(target){
-        tabLi.removeClass("on")
+        target.siblings().removeClass("on")
         target.addClass("on");
     }
+    function tabContFunc(target){
 
-    function tabContFunc(){
-        for(var i = 0; i<tabLi.length; i++){
-            if(tabLi.eq(i).hasClass('on')){
+        for(var i = 0; i<target.siblings().length+1; i++){
+            
+            var tabUl = target.parent('ul.tab_button_wrapper');
+            var tabCont = tabUl.siblings('div.tab_cont_wrap').children('div.tab_cont');
+
+            tabCont.removeClass('on');
+            
+            if(tabUl.children('li').eq(i).hasClass('on')){
                 tabCont.eq(i).addClass('on')
                 return true
             }
         }
     }
-
     function tabLiClickFunc(){
         tabLi.on("click",function(){
             tabLiToggle($(this))
 
-            tabCont.removeClass("on")
-            tabContFunc();
+            tabContFunc($(this));
 
         })
     }
-
     return{
         init: init
     }
